011a3167614a1f4cdd0d9094bf7005adffcd87c3
Damien <damien.flury@gmail.com>
2023-04-04 11:23:19 +0200

    Add background Image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,16 @@ export default class Home extends Lightning.Component {
         h: 1080,
         color: 0xff000000,
       },
+      BackgroundImage: {
+        rect: true,
+        shader: {
+          type: Lightning.shaders.Vignette,
+          magnitude: 3,
+          intensity: 0.3,
+        },
+        w: 1920,
+        h: 1080,
+      },
       Border: {
         rect: true,
         w: 276,
@@ -29,15 +39,42 @@ export default class Home extends Lightning.Component {
         type: List,
         direction: "row",
       },
+      Title: {
+        y: 800,
+        x: 960,
+        mount: 0.5,
+        text: {
+          text: "",
+        },
+      },
+      Plot: {
+        y: 860,
+        x: 960,
+        w: 540,
+        mountX: 0.5,
+        text: {
+          text: "",
+          fontSize: 20,
+        },
+      },
     };
   }
   pageTransition() {
     return "up";
   }
+
+  setDescription() {
+    this.tag("Plot").patch({
+      text: {
+        text: this.movies[this.tag("Movies").index].overview,
+      },
+    });
+  }
   async _init() {
     this.borderIndex = 0;
     const data = await fetchMovies();
     this.movies = data.results;
+    this.setDescription();
     this.tag("Movies").add(
       data.results.map((m) => ({
         src: `https://image.tmdb.org/t/p/original${m.poster_path}`,
@@ -48,10 +85,34 @@ export default class Home extends Lightning.Component {
       }))
     );
     this.tag("Movies").setIndex(0);
+    this.tag("Title").patch({
+      text: {
+        text: this.movies[0].title,
+      },
+    });
+
+    this.setBackgroundImage(this.movies[0].backdrop_path);
   }
+
+  setBackgroundImage(src) {
+    this.tag("BackgroundImage").patch({
+      src: `https://image.tmdb.org/t/p/original${src}`,
+    });
+  }
+
   _handleRight() {
     this.tag("Movies").setIndex(this.tag("Movies").index + 1);
     const idx = this.tag("Movies").index;
+
+    this.setDescription();
+    this.tag("Title").patch({
+      text: {
+        text: this.movies[idx].title,
+      },
+    });
+
+    this.setBackgroundImage(this.movies[idx].backdrop_path);
+
     if (this.borderIndex > 5) {
       return;
     }
@@ -59,7 +120,6 @@ export default class Home extends Lightning.Component {
 
     this.tag("Border").patch({
       smooth: {
-        // x: this.borderIndex === 7 ? 1660 : this.borderIndex * 270,
         x: this.borderIndex * 276,
       },
     });
@@ -68,6 +128,15 @@ export default class Home extends Lightning.Component {
   _handleLeft() {
     this.tag("Movies").setIndex(this.tag("Movies").index - 1);
     const idx = this.tag("Movies").index;
+    this.setDescription();
+    this.tag("Title").patch({
+      text: {
+        text: this.movies[idx].title,
+      },
+    });
+
+    this.setBackgroundImage(this.movies[idx].backdrop_path);
+
     if (this.borderIndex === 0) {
       return;
     }
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,17 +1,76 @@
-import { Lightning, Router } from "@lightningjs/sdk";
+import { Lightning } from "@lightningjs/sdk";
 import { fetchMovieById } from "../lib/api";
 
 export default class Movie extends Lightning.Component {
   static _template() {
     return {
-      rect: true,
-      w: 1920,
-      h: 1080,
-      color: 0xff000000,
+      Background: {
+        rect: true,
+        w: 1920,
+        h: 1080,
+        color: 0xff000000,
+      },
+      BackgroundImage: {
+        rect: true,
+        shader: {
+          type: Lightning.shaders.Vignette,
+          magnitude: 3,
+          intensity: 0.3,
+        },
+        w: 1920,
+        h: 1080,
+      },
+      Title: {
+        x: 960,
+        y: 120,
+        mount: 0.5,
+      },
+      Description: {
+        x: 960,
+        y: 420,
+        w: 820,
+        mount: 0.5,
+        text: {
+          fontSize: 20,
+          lineHeight: 20,
+        },
+      },
+      ReleaseDate: {
+        x: 960,
+        y: 240,
+        mount: 0.5,
+        text: {
+          fontSize: 25,
+        },
+      },
     };
   }
 
+  /**
+   * @param {{ id: { id: string }; }} args
+   */
   set params(args) {
-    fetchMovieById(args.id).then(console.log);
+    fetchMovieById(args.id).then((res) => {
+      this.tag("BackgroundImage").patch({
+        src: `https://image.tmdb.org/t/p/original${res.backdrop_path}`,
+      });
+      this.tag("Title").patch({
+        text: {
+          text: res.title,
+        },
+      });
+      this.tag("Description").patch({
+        text: {
+          text: res.overview,
+        },
+      });
+      this.tag("ReleaseDate").patch({
+        text: {
+          text: new Intl.DateTimeFormat("en-GB", { dateStyle: "full" }).format(
+            new Date(res.release_date)
+          ),
+        },
+      });
+    });
   }
 }
